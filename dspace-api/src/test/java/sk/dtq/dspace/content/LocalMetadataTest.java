81e5a5f65869fe8998cae65f9adb330a95282c7d
MilanMajchrák <milan.majchak@dataquest.sk>
2022-04-27 09:01:30 +0200

    Created unit tests for the new local metadata types

---
+++ dspace-api/src/test/java/sk/dtq/dspace/content/LocalMetadataTest.java
@@ -0,0 +1,44 @@
+package sk.dtq.dspace.content;
+
+import org.dspace.AbstractUnitTest;
+import org.dspace.content.MetadataField;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.MetadataFieldService;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+/**
+ * Unit Tests for class MetadataFieldTest
+ *
+ * @author milanmajchrak
+ */
+public class LocalMetadataTest extends AbstractUnitTest {
+
+    private MetadataFieldService metadataFieldService = ContentServiceFactory.getInstance().getMetadataFieldService();
+
+    /**
+     * Test of existing custom metadata field `local.contact.person`
+     */
+    @Test
+    public void existContactPerson() throws Exception {
+        MetadataField field = metadataFieldService.findByString(context, "local.contact.person",
+                '.');
+
+        assertThat("existContactPerson 0", field, notNullValue());
+    }
+
+    /**
+     * Test of existing custom metadata field `local.sponsor.null`
+     */
+    @Test
+    public void existSponsor() throws Exception {
+        MetadataField field = metadataFieldService.findByString(context, "local.sponsor",
+                '.');
+
+        assertThat("existSponsor 0", field, notNullValue());
+    }
+
+}
