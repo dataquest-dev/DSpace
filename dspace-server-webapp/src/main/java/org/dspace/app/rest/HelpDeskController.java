09fc49335f4c8e2582c22bdaf644ff42706a70ec
MilanMajchrák <milan.majchak@dataquest.sk>
2022-08-11 19:49:19 +0200

    Updated ItemConverter to return necessary metadata for the tombstone.

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/HelpDeskController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/HelpDeskController.java
@@ -2,10 +2,10 @@ package org.dspace.app.rest;
 
 import org.dspace.services.ConfigurationService;
 import org.json.JSONObject;
-import org.json.JSONString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +23,13 @@ public class HelpDeskController {
     @GetMapping
     public ResponseEntity<String> getHelpDeskMail() {
         JSONObject jsonHelpDesk = new JSONObject();
-        JSONObject payload = new JSONObject();
 
-        jsonHelpDesk.put("mail", this.configurationService.getProperty("lr.help.mail"));
-        payload.put("payload", jsonHelpDesk);
-        return new ResponseEntity<>(payload.toString(), HttpStatus.OK);
+        String helpDeskMail = this.configurationService.getProperty("lr.help.mail");
+        if (ObjectUtils.isEmpty(helpDeskMail)) {
+            new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        jsonHelpDesk.put("mail", helpDeskMail);
+        return new ResponseEntity<>(jsonHelpDesk.toString(), HttpStatus.OK);
     }
 }
