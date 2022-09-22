53bf884c52aba33d0691be62c3a849cc9df8a18c
milanmajchrak <90026355+milanmajchrak@users.noreply.github.com>
2022-09-22 15:33:21 +0200

    Feature/lf 4 license administrator-2-dtq-dev-tmp (#150)
    * Initial commit
    
    * Added some ClarinLicense classes
    
    * Added some ClarinLicense classes
    
    * Added some ClarinLicense classes
    
    * Created ClarinLicenseLabelList and other classes. It throws ClassNotFound exception
    
    * Clarin License has ClarinLicenseLabel classes in the response
    
    * Return the clarinLicense with extended labels and another custom data
    
    * Return the clarinLicense with extended labels and another custom data
    
    * Created findAll method in the ClarinLicenseLabelRestRepository
    
    * Cannot map HashMap to the ClarinLicenseRest
    
    * Created Endpoint and test for defining a new clarin license
    
    * Created Endpoint and tests for Deleting the Clarin Licenses
    
    * Created endpoint for updating licenses with tests
    
    * Created endpoint for creating Clarin License Labels with tests
    
    * Added docs
    
    * fixed checkstyle violations
    
    Co-authored-by: MilanMajchrák <milan.majchak@dataquest.sk>

---
+++ dspace-api/src/main/java/org/dspace/content/factory/ClarinServiceFactoryImpl.java
@@ -0,0 +1,37 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
+package org.dspace.content.factory;
+
+import org.dspace.content.service.clarin.ClarinLicenseLabelService;
+import org.dspace.content.service.clarin.ClarinLicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Factory implementation to get services for the clarin package, use ClarinServiceFactory.getInstance()
+ * to retrieve an implementation
+ *
+ * @author Milan Majchrak (milan.majchrak at dataquest.sk)
+ */
+public class ClarinServiceFactoryImpl extends ClarinServiceFactory {
+
+    @Autowired(required = true)
+    private ClarinLicenseService clarinLicenseService;
+
+    @Autowired(required = true)
+    private ClarinLicenseLabelService clarinLicenseLabelService;
+
+    @Override
+    public ClarinLicenseService getClarinLicenseService() {
+        return clarinLicenseService;
+    }
+
+    @Override
+    public ClarinLicenseLabelService getClarinLicenseLabelService() {
+        return clarinLicenseLabelService;
+    }
+}
--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.2_2022.07.28__Upgrade_to_Lindat_Clarin_schema.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.2_2022.07.28__Upgrade_to_Lindat_Clarin_schema.sql
@@ -6,6 +6,325 @@
 -- http://www.dspace.org/license/
 --
 
+-- HANDLE TABLE
 ALTER TABLE handle ADD url varchar(2048);
 ALTER TABLE handle ADD dead BOOL;
 ALTER TABLE handle ADD dead_since TIMESTAMP WITH TIME ZONE;
+
+
+-- LICENSES
+--
+-- Name: license_definition; Type: TABLE; Schema: public; Owner: dspace; Tablespace:
+--
+
+CREATE TABLE license_definition (
+    license_id integer NOT NULL,
+    name varchar(256),
+    definition varchar(256),
+    eperson_id integer,
+    label_id integer,
+    created_on timestamp,
+    confirmation integer DEFAULT 0,
+    required_info varchar(64)
+);
+
+ALTER TABLE public.license_definition OWNER TO dspace;
+
+--
+-- Name: license_definition_license_id_seq; Type: SEQUENCE; Schema: public; Owner: dspace
+--
+
+CREATE SEQUENCE license_definition_license_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+ALTER TABLE public.license_definition_license_id_seq OWNER TO dspace;
+
+--
+-- Name: license_definition_license_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: dspace
+--
+
+ALTER SEQUENCE license_definition_license_id_seq OWNED BY license_definition.license_id;
+
+--
+-- Name: license_label; Type: TABLE; Schema: public; Owner: dspace; Tablespace:
+--
+
+CREATE TABLE license_label (
+    label_id integer NOT NULL,
+    label varchar(5),
+    title varchar(180),
+    icon bytea,
+    is_extended boolean DEFAULT false
+);
+
+
+ALTER TABLE public.license_label OWNER TO dspace;
+
+--
+-- Name: license_label_extended_mapping; Type: TABLE; Schema: public; Owner: dspace; Tablespace:
+--
+
+CREATE TABLE license_label_extended_mapping (
+    mapping_id integer NOT NULL,
+    license_id integer,
+    label_id integer
+);
+
+ALTER TABLE public.license_label_extended_mapping OWNER TO dspace;
+
+--
+-- Name: license_label_extended_mapping_mapping_id_seq; Type: SEQUENCE; Schema: public; Owner: dspace
+--
+
+CREATE SEQUENCE license_label_extended_mapping_mapping_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.license_label_extended_mapping_mapping_id_seq OWNER TO dspace;
+
+--
+-- Name: license_label_extended_mapping_mapping_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: dspace
+--
+
+ALTER SEQUENCE license_label_extended_mapping_mapping_id_seq OWNED BY license_label_extended_mapping.mapping_id;
+
+
+--
+-- Name: license_label_extended_mapping_mapping_id_seq; Type: SEQUENCE SET; Schema: public; Owner: dspace
+--
+
+SELECT pg_catalog.setval('license_label_extended_mapping_mapping_id_seq', 991137, true);
+
+--
+-- Name: license_label_label_id_seq; Type: SEQUENCE; Schema: public; Owner: dspace
+--
+
+CREATE SEQUENCE license_label_label_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.license_label_label_id_seq OWNER TO dspace;
+
+--
+-- Name: license_label_label_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: dspace
+--
+
+ALTER SEQUENCE license_label_label_id_seq OWNED BY license_label.label_id;
+
+
+--
+-- Name: license_label_label_id_seq; Type: SEQUENCE SET; Schema: public; Owner: dspace
+--
+
+SELECT pg_catalog.setval('license_label_label_id_seq', 19, true);
+
+--
+-- Name: license_resource_mapping; Type: TABLE; Schema: public; Owner: dspace; Tablespace:
+--
+
+CREATE TABLE license_resource_mapping (
+    mapping_id integer NOT NULL,
+    bitstream_id integer,
+    license_id integer,
+    active boolean DEFAULT true
+);
+
+
+ALTER TABLE public.license_resource_mapping OWNER TO dspace;
+
+--
+-- Name: license_resource_mapping_mapping_id_seq; Type: SEQUENCE; Schema: public; Owner: dspace
+--
+
+CREATE SEQUENCE license_resource_mapping_mapping_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.license_resource_mapping_mapping_id_seq OWNER TO dspace;
+
+--
+-- Name: license_resource_mapping_mapping_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: dspace
+--
+
+ALTER SEQUENCE license_resource_mapping_mapping_id_seq OWNED BY license_resource_mapping.mapping_id;
+
+
+--
+-- Name: license_resource_mapping_mapping_id_seq; Type: SEQUENCE SET; Schema: public; Owner: dspace
+--
+
+SELECT pg_catalog.setval('license_resource_mapping_mapping_id_seq', 1382, true);
+
+
+--
+-- Name: license_resource_user_allowance; Type: TABLE; Schema: public; Owner: dspace; Tablespace:
+--
+
+CREATE TABLE license_resource_user_allowance (
+    transaction_id integer NOT NULL,
+    eperson_id integer,
+    mapping_id integer,
+    created_on timestamp,
+    token char(32)
+);
+
+ALTER TABLE public.license_resource_user_allowance OWNER TO dspace;
+
+--
+-- Name: license_resource_user_allowance_transaction_id_seq; Type: SEQUENCE; Schema: public; Owner: dspace
+--
+
+CREATE SEQUENCE license_resource_user_allowance_transaction_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.license_resource_user_allowance_transaction_id_seq OWNER TO dspace;
+
+--
+-- Name: license_resource_user_allowance_transaction_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: dspace
+--
+
+ALTER SEQUENCE license_resource_user_allowance_transaction_id_seq OWNED BY license_resource_user_allowance.transaction_id;
+
+
+--
+-- Name: license_resource_user_allowance_transaction_id_seq; Type: SEQUENCE SET; Schema: public; Owner: dspace
+--
+
+SELECT pg_catalog.setval('license_resource_user_allowance_transaction_id_seq', 241, true);
+
+--
+-- Name: license_id; Type: DEFAULT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_definition ALTER COLUMN license_id SET DEFAULT nextval('license_definition_license_id_seq'::regclass);
+
+
+--
+-- Name: label_id; Type: DEFAULT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_label ALTER COLUMN label_id SET DEFAULT nextval('license_label_label_id_seq'::regclass);
+
+
+--
+-- Name: mapping_id; Type: DEFAULT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_label_extended_mapping ALTER COLUMN mapping_id SET DEFAULT nextval('license_label_extended_mapping_mapping_id_seq'::regclass);
+
+
+--
+-- Name: mapping_id; Type: DEFAULT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_resource_mapping ALTER COLUMN mapping_id SET DEFAULT nextval('license_resource_mapping_mapping_id_seq'::regclass);
+
+
+--
+-- Name: transaction_id; Type: DEFAULT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_resource_user_allowance ALTER COLUMN transaction_id SET DEFAULT nextval('license_resource_user_allowance_transaction_id_seq'::regclass);
+
+--
+-- Name: license_definition_pkey; Type: CONSTRAINT; Schema: public; Owner: dspace; Tablespace:
+--
+
+ALTER TABLE ONLY license_definition
+    ADD CONSTRAINT license_definition_pkey PRIMARY KEY (license_id);
+
+
+--
+-- Name: license_label_extended_mapping_pkey; Type: CONSTRAINT; Schema: public; Owner: dspace; Tablespace:
+--
+
+ALTER TABLE ONLY license_label_extended_mapping
+    ADD CONSTRAINT license_label_extended_mapping_pkey PRIMARY KEY (mapping_id);
+
+
+--
+-- Name: license_label_pkey; Type: CONSTRAINT; Schema: public; Owner: dspace; Tablespace:
+--
+
+ALTER TABLE ONLY license_label
+    ADD CONSTRAINT license_label_pkey PRIMARY KEY (label_id);
+
+
+--
+-- Name: license_resource_mapping_pkey; Type: CONSTRAINT; Schema: public; Owner: dspace; Tablespace:
+--
+
+ALTER TABLE ONLY license_resource_mapping
+    ADD CONSTRAINT license_resource_mapping_pkey PRIMARY KEY (mapping_id);
+
+
+--
+-- Name: license_resource_user_allowance_pkey; Type: CONSTRAINT; Schema: public; Owner: dspace; Tablespace:
+--
+
+ALTER TABLE ONLY license_resource_user_allowance
+    ADD CONSTRAINT license_resource_user_allowance_pkey PRIMARY KEY (transaction_id);
+
+
+CREATE UNIQUE INDEX license_definition_license_id_key ON license_definition USING btree (name);
+
+
+--
+-- Name: license_definition_license_label_extended_mapping_fk; Type: FK CONSTRAINT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_label_extended_mapping
+    ADD CONSTRAINT license_definition_license_label_extended_mapping_fk FOREIGN KEY (license_id) REFERENCES license_definition(license_id) ON DELETE CASCADE;
+
+
+--
+-- Name: license_definition_license_resource_mapping_fk; Type: FK CONSTRAINT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_resource_mapping
+    ADD CONSTRAINT license_definition_license_resource_mapping_fk FOREIGN KEY (license_id) REFERENCES license_definition(license_id) ON DELETE CASCADE;
+
+
+--
+-- Name: license_label_license_definition_fk; Type: FK CONSTRAINT; Schema: public; Owner: dspace
+--
+
+--ALTER TABLE ONLY license_definition
+--    ADD CONSTRAINT license_label_license_definition_fk FOREIGN KEY (label_id) REFERENCES license_label(label_id);
+
+
+--
+-- Name: license_label_license_label_extended_mapping_fk; Type: FK CONSTRAINT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_label_extended_mapping
+    ADD CONSTRAINT license_label_license_label_extended_mapping_fk FOREIGN KEY (label_id) REFERENCES license_label(label_id) ON DELETE CASCADE;
+
+
+--
+-- Name: license_resource_mapping_license_resource_user_allowance_fk; Type: FK CONSTRAINT; Schema: public; Owner: dspace
+--
+
+ALTER TABLE ONLY license_resource_user_allowance
+    ADD CONSTRAINT license_resource_mapping_license_resource_user_allowance_fk FOREIGN KEY (mapping_id) REFERENCES license_resource_mapping(mapping_id) ON UPDATE CASCADE ON DELETE CASCADE;
