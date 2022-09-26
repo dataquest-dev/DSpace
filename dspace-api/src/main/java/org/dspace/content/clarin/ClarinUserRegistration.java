e7095d93a3c65ac6759d9b61846bf5e4029a147c
MilanMajchrák <milan.majchak@dataquest.sk>
2022-09-26 09:27:30 +0200

    Added license headers

--- dspace-api/src/main/java/org/dspace/content/clarin/ClarinUserRegistration.java
+++ dspace-api/src/main/java/org/dspace/content/clarin/ClarinUserRegistration.java
@@ -1,10 +1,10 @@
-///**
-// * The contents of this file are subject to the license and copyright
-// * detailed in the LICENSE and NOTICE files at the root of the source
-// * tree and available online at
-// *
-// * http://www.dspace.org/license/
-// */
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 //package org.dspace.content.clarin;
 //
 //import org.dspace.core.ReloadableEntity;
