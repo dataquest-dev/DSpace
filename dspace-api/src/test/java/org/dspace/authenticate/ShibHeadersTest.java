4bf96c91f3580612a30ec8d5903b361504f299ff
MilanMajchrák <milan.majchak@dataquest.sk>
2022-12-19 10:54:19 +0100

    Added docs for the ShibHeadersTest

--- dspace-api/src/test/java/org/dspace/authenticate/ShibHeadersTest.java
+++ dspace-api/src/test/java/org/dspace/authenticate/ShibHeadersTest.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.authenticate;
 
 import org.dspace.AbstractUnitTest;
@@ -8,8 +15,17 @@ import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Unit test for the class ShibHeaders. In this class is testing the added constructor the create the ShibHeaders
+ * object from the String.
+ *
+ * @author Milan Majchrak (milan.majchrak at dataquest.sk)
+ */
 public class ShibHeadersTest extends AbstractUnitTest {
 
+    /**
+     * Test constructor to create ShibHeaders object from the String.
+     */
     @Test
     public void testParsingStringHeaders() {
         String shibHeadersString = "shib-netid=123456\nshib-identity-provider=Test Idp\n" +
@@ -18,5 +34,4 @@ public class ShibHeadersTest extends AbstractUnitTest {
         ShibHeaders shibHeaders = new ShibHeaders(shibHeadersString);
         assertEquals(Objects.nonNull(shibHeaders), true);
     }
-
 }
