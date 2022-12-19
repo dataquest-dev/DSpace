4ea2a8f34e3eeee4d984708ecf66af48f0e57c4f
MilanMajchrák <milan.majchak@dataquest.sk>
2022-12-19 11:17:30 +0100

    Added docs for the ClarinAutoRegistrationController

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/Application.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/Application.java
@@ -174,9 +174,7 @@ public class Application extends SpringBootServletInitializer {
                             // for our Access-Control-Allow-Origin header
                             .allowCredentials(corsAllowCredentials).allowedOrigins(corsAllowedOrigins)
                             // Allow list of request preflight headers allowed to be sent to us from the client
-                            .allowedHeaders("Accept", "Authorization", "Content-Type", "Origin", "X-On-Behalf-Of",
-                                "X-Requested-With", "X-XSRF-TOKEN", "X-CORRELATION-ID", "X-REFERRER",
-                                    "SHIB-GIVENNAME", "SHIB-SURNAME", "SHIB-MAIL", "SHIB-NETID")
+                            .allowedHeaders("Accept", "Authorization", "Content-Type", "Origin", "X-On-Behalf-Of")
                             // Allow list of response headers allowed to be sent by us (the server) to the client
                             .exposedHeaders("Authorization", "DSPACE-XSRF-TOKEN", "Location", "WWW-Authenticate");
                 }
--- dspace-server-webapp/src/main/java/org/dspace/app/rest/ClarinAutoRegistrationController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/ClarinAutoRegistrationController.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.app.rest;
 
 import org.apache.commons.lang3.StringUtils;
@@ -29,6 +36,12 @@ import java.sql.SQLException;
 import java.util.Locale;
 import java.util.Objects;
 
+/**
+ * If the Shibboleth authentication failed because the IdP hasn't sent the `SHIB-EMAIL` header.
+ * Controller for sending the email with verification token to the user and confirming the token by it.
+ *
+ * @author Milan Majchrak (milan.majchrak at dataquest.sk)
+ */
 @RequestMapping(value = "/api/autoregistration")
 @RestController
 public class ClarinAutoRegistrationController {
@@ -42,6 +55,12 @@ public class ClarinAutoRegistrationController {
     @Autowired
     EPersonService ePersonService;
 
+    /**
+     * Send the email with the verification token to the user.
+     * @param netid of the user, this information is retrieved from the IdP.
+     * @param email which the user has filled in.
+     * @return Successful response or an error message.
+     */
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity sendEmail(HttpServletRequest request, HttpServletResponse response,
                                     @RequestParam("netid") String netid,
@@ -53,8 +72,12 @@ public class ClarinAutoRegistrationController {
             throw new RuntimeException("Cannot obtain the context from the request");
         }
 
+        // The verification token object is created when the Shibboleth authentication is failed in the
+        // ClarinShibbolethLoginFilter. It is created with the user `netid` and `shib_headers` which are passed
+        // from the IdP.
         ClarinVerificationToken clarinVerificationToken = clarinVerificationTokenService.findByNetID(context, netid);
         if (Objects.isNull(clarinVerificationToken)) {
+            // The verification token doesn't exist.
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Cannot load the clarin verification " +
                     "token class by net id: " + netid);
             return null;
@@ -74,6 +97,7 @@ public class ClarinAutoRegistrationController {
         clarinVerificationTokenService.update(context, clarinVerificationToken);
         context.commit();
 
+        // Compose the url with the verification token. The user will be redirected to the UI.
         String autoregistrationURL = uiUrl + "/login/autoregistration?verification-token=" + verificationToken;
         try {
             Locale locale = context.getCurrentLocale();
@@ -90,6 +114,13 @@ public class ClarinAutoRegistrationController {
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Confirm the verification token which the user received from the email. If the verification token is
+     * valid the user is registered and signed in or just signed in (if it exists). After successful
+     * signing in the verification token object is removed from the database.
+     * @param token to verify.
+     * @return Successful response or an error message.
+     */
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity confirmEmail(HttpServletRequest request, HttpServletResponse response,
                                        @RequestParam("verification-token") String token) throws IOException,
@@ -100,13 +131,15 @@ public class ClarinAutoRegistrationController {
             throw new RuntimeException("Cannot obtain the context from the request");
         }
 
-        // Check if the token is valid
+        // Check if the token is valid (if it exists in the database)
         ClarinVerificationToken clarinVerificationToken = clarinVerificationTokenService.findByToken(context, token);
         if (Objects.isNull(clarinVerificationToken)) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND, "Verification token doesn't exists.");
             return null;
         }
 
+        // Set request attribute `shib.headers` for authentication method.
+        // These headers are retrieved in the ClarinShibAuthentication.authenticate method.
         request.setAttribute("shib.headers", clarinVerificationToken.getShibHeaders());
         try {
             new ClarinShibAuthentication().authenticate(context, "", "", "", request);
@@ -118,8 +151,7 @@ public class ClarinAutoRegistrationController {
         }
         context.commit();
 
-        // If the Authentication was successful the Eperson should be found, because authentication register a new
-        // user if he doesn't exist.
+        // If the Authentication was successful the Eperson should be found.
         EPerson ePerson = ePersonService.findByNetid(context, clarinVerificationToken.getePersonNetID());
         if (Objects.isNull(ePerson)) {
             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
