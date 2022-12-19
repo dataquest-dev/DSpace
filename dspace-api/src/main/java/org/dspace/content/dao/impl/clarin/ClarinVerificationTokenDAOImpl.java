b478dac7679023fdf7d0952074e373f8fd906f71
MilanMajchrák <milan.majchak@dataquest.sk>
2022-12-19 10:50:44 +0100

    Added doc to the ClarinVerificationTokenDAO and ClarinVerificationTokenDAOImpl

--- dspace-api/src/main/java/org/dspace/content/dao/impl/clarin/ClarinVerificationTokenDAOImpl.java
+++ dspace-api/src/main/java/org/dspace/content/dao/impl/clarin/ClarinVerificationTokenDAOImpl.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.content.dao.impl.clarin;
 
 import org.dspace.content.clarin.ClarinLicense;
@@ -10,6 +17,13 @@ import org.dspace.core.Context;
 import javax.persistence.Query;
 import java.sql.SQLException;
 
+/**
+ * Hibernate implementation of the Database Access Object interface class for the ClarinVerificationToken object.
+ * This class is responsible for all database calls for the ClarinVerificationToken object and is autowired by spring
+ * This class should never be accessed directly.
+ *
+ * @author Milan Majchrak (milan.majchrak at dataquest.sk)
+ */
 public class ClarinVerificationTokenDAOImpl extends AbstractHibernateDAO<ClarinVerificationToken>
         implements ClarinVerificationTokenDAO {
 
