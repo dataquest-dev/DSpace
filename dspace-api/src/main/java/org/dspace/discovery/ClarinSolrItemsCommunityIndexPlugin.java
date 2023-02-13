7a0fec2bc9a0f5a3911c4bed7b45580fcda80f8c
MilanMajchrák <milan.majchak@dataquest.sk>
2023-02-13 09:49:14 +0100

    Add license header check and update signature

--- dspace-api/src/main/java/org/dspace/discovery/ClarinSolrItemsCommunityIndexPlugin.java
+++ dspace-api/src/main/java/org/dspace/discovery/ClarinSolrItemsCommunityIndexPlugin.java
@@ -1,3 +1,10 @@
+/**
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ * http://www.dspace.org/license/
+ */
 package org.dspace.discovery;
 
 import java.sql.SQLException;
@@ -13,6 +20,11 @@ import org.dspace.content.Item;
 import org.dspace.core.Context;
 import org.dspace.discovery.indexobject.IndexableItem;
 
+/**
+ * Plugin for indexing the Items community. It helps search the Item by the community.
+ *
+ * @author Milan Majchrak (milan.majchrak at dataquest.sk)
+ */
 public class ClarinSolrItemsCommunityIndexPlugin implements SolrServiceIndexPlugin {
 
     private static final Logger log = org.apache.logging.log4j.LogManager
