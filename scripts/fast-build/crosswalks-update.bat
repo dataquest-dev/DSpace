5453b7831eb54d350bce3f39cd9e5f015766bb0f
MilanMajchrák <milan.majchak@dataquest.sk>
2023-04-13 12:57:07 +0200

    Added fast building script for oai-pmh package

--- scripts/fast-build/crosswalks-update.bat
+++ scripts/fast-build/crosswalks-update.bat
@@ -1,10 +1,6 @@
 call ..\envs\__basic.bat
 
-set tomcat_bin=%tomcat%\bin\
-
 call tomcat\stop.bat
-
-cd %dspace_source%\scripts\fast-build\
 
 xcopy /e /h /i /q /y %dspace_source%\dspace\config\crosswalks\oai\metadataFormats\ %dspace_application%\config\crosswalks\oai\metadataFormats\
 
