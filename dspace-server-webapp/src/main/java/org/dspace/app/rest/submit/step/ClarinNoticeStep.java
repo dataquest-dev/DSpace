ebd93233ca5259a31cefd0979b8ec45d2bb0528d
MilanMajchrák <milan.majchak@dataquest.sk>
2023-04-19 17:35:18 +0200

    Fixed checkstyle issues.

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/submit/step/ClarinNoticeStep.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/submit/step/ClarinNoticeStep.java
@@ -7,23 +7,15 @@
  */
 package org.dspace.app.rest.submit.step;
 
-import org.atteo.evo.inflector.English;
-import org.dspace.app.rest.exception.UnprocessableEntityException;
-import org.dspace.app.rest.model.BitstreamRest;
+import java.io.Serializable;
+import javax.servlet.http.HttpServletRequest;
+
 import org.dspace.app.rest.model.patch.Operation;
-import org.dspace.app.rest.model.step.DataLicense;
 import org.dspace.app.rest.submit.AbstractProcessingStep;
 import org.dspace.app.rest.submit.SubmissionService;
-import org.dspace.app.rest.submit.factory.PatchOperationFactory;
-import org.dspace.app.rest.submit.factory.impl.PatchOperation;
 import org.dspace.app.util.SubmissionStepConfig;
-import org.dspace.content.Bitstream;
 import org.dspace.content.InProgressSubmission;
-import org.dspace.core.Constants;
 import org.dspace.core.Context;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.Serializable;
 
 /**
  * Clarin Notice step for DSpace Spring Rest. This step will show addition information about the current collection
@@ -33,17 +25,14 @@ import java.io.Serializable;
  */
 public class ClarinNoticeStep extends AbstractProcessingStep {
 
-    private static final String DCTERMS_RIGHTSDATE = "dcterms.accessRights";
-
     @Override
-    public <T extends Serializable> T getData(SubmissionService submissionService, InProgressSubmission obj, SubmissionStepConfig config) throws Exception {
-        System.out.println("I am here2");
+    public <T extends Serializable> T getData(SubmissionService submissionService, InProgressSubmission obj,
+                                              SubmissionStepConfig config) throws Exception {
         return null;
     }
 
     @Override
     public void doPatchProcessing(Context context, HttpServletRequest currentRequest, InProgressSubmission source,
             Operation op, SubmissionStepConfig stepConf) throws Exception {
-        System.out.println("I am here");
     }
 }
