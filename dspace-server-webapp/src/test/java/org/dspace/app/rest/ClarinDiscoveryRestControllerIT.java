571e219abe5be6f39975f571f6d36f0625ec250a
milanmajchrak <milan.majchrak@dataquest.sk>
2023-08-07 13:56:10 +0200

    Allowed DiscoveryIT

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ClarinDiscoveryRestControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ClarinDiscoveryRestControllerIT.java
@@ -76,8 +76,6 @@ import org.springframework.http.MediaType;
  *
  * @author Milan Majchrak (milan.majchrak at dataquest.sk)
  */
-// Ignore for the TUL customer
-@Ignore
 public class ClarinDiscoveryRestControllerIT extends AbstractControllerIntegrationTest {
     @Autowired
     ConfigurationService configurationService;
