adaddd0f914fa2f4488ec025a3667a43a69ac037
milanmajchrak <milan.majchrak@dataquest.sk>
2023-08-31 12:29:07 +0200

    Refactored code.

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/MetadataConverter.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/converter/MetadataConverter.java
@@ -139,20 +139,39 @@ public class MetadataConverter implements DSpaceConverter<MetadataValueList, Met
             String element = seq[1];
             String qualifier = seq.length == 3 ? seq[2] : null;
             for (MetadataValueRest mvr: entry.getValue()) {
-                // Use `place` argument only for ItemService because some services doesn't have implemented
-                // `addMetadata` method with place argument.
-                // `place` is important because of importing Items. Without it the Items metadata could be in the
-                // wrong sequence e.g., files in the item.
-                if (dsoService instanceof ItemService) {
-                    dsoService.addMetadata(context, dso, schema, element, qualifier, mvr.getLanguage(),
-                            mvr.getValue(), mvr.getAuthority(), mvr.getConfidence(), mvr.getPlace());
-                } else {
-                    dsoService.addMetadata(context, dso, schema, element, qualifier, mvr.getLanguage(),
-                            mvr.getValue(), mvr.getAuthority(), mvr.getConfidence());
-                }
+                addMetadataByService(dsoService, context, dso, schema, element, qualifier, mvr);
             }
         }
         dsoService.update(context, dso);
+    }
+
+    /**
+     * Call `addMetadata` method by arbitrary dso service. ItemService calls `addMetadata` method with more
+     * arguments than other services.
+     *
+     * @param dsoService service with implemented `addMetadata` method
+     * @param context the context to use.
+     * @param dso the DSpace object.
+     * @param schema name of the metadata schema
+     * @param element name of the metadata element
+     * @param qualifier name of the metadata qualifier
+     * @param mvr MetadataValueRest object
+     * @throws SQLException if a database error occurs.
+     */
+    private <T extends DSpaceObject> void addMetadataByService(DSpaceObjectService<T> dsoService, Context context,
+                                                               T dso, String schema, String element, String qualifier,
+                                                      MetadataValueRest mvr) throws SQLException {
+        // Use `place` argument only for ItemService because some services doesn't have implemented
+        // `addMetadata` method with `place` argument.
+        // `place` is important because of importing Items. Without it the Items metadata could be in the
+        // wrong sequence e.g., authors of the item.
+        if (dsoService instanceof ItemService) {
+            dsoService.addMetadata(context, dso, schema, element, qualifier, mvr.getLanguage(),
+                    mvr.getValue(), mvr.getAuthority(), mvr.getConfidence(), mvr.getPlace());
+        } else {
+            dsoService.addMetadata(context, dso, schema, element, qualifier, mvr.getLanguage(),
+                    mvr.getValue(), mvr.getAuthority(), mvr.getConfidence());
+        }
     }
 
 }
