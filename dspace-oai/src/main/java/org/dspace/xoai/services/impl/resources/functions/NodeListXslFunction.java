255b068a452e779e6c164741ded05e5534b41f3b
MajoBerger <marian.berger@dataquest.sk>
2024-07-24 14:32:43 +0200

    add issues and todo comments

--- dspace-oai/src/main/java/org/dspace/xoai/services/impl/resources/functions/NodeListXslFunction.java
+++ dspace-oai/src/main/java/org/dspace/xoai/services/impl/resources/functions/NodeListXslFunction.java
@@ -78,6 +78,8 @@ public abstract class NodeListXslFunction implements ExtensionFunction {
 
 
         NodeList nodeList = getNodeList(val);
+        // TODO: 7/24/2024 (mb) Below is attempted fix that does not work yet.
+        // see https://github.com/dataquest-dev/DSpace/issues/709
 //        Node oneNode = nodeList.item(0);
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
