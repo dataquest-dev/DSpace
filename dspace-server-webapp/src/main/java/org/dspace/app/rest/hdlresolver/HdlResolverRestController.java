288173cfb10d0162b80bbd86697138270a73c5af
Paurikova2 <michaela.paurikova@dataquest.sk>
2024-10-07 23:25:45 +0200

    checkstyle violations

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/hdlresolver/HdlResolverRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/hdlresolver/HdlResolverRestController.java
@@ -7,20 +7,15 @@
  */
 package org.dspace.app.rest.hdlresolver;
 
-import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.handle.hdllib.Encoder;
 import net.handle.hdllib.HandleException;
-import net.handle.hdllib.HandleValue;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
