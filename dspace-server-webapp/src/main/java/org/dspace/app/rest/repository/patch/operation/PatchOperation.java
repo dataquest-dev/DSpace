64d7eae5c4f9c7eb27de361df603c0a55ad4f0b2
Paurikova2 <michaela.paurikova@dataquest.sk>
2024-11-20 10:30:05 +0100

    removed unused services

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/patch/operation/PatchOperation.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/patch/operation/PatchOperation.java
@@ -12,8 +12,6 @@ import java.sql.SQLException;
 import org.apache.commons.lang3.BooleanUtils;
 import org.dspace.app.rest.exception.DSpaceBadRequestException;
 import org.dspace.app.rest.model.patch.Operation;
-import org.dspace.content.service.InstallItemService;
-import org.dspace.content.service.ItemService;
 import org.dspace.core.Context;
 import org.dspace.core.ProvenanceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,12 +27,6 @@ public abstract class PatchOperation<M> {
     public static final String OPERATION_COPY = "copy";
     public static final String OPERATION_MOVE = "move";
     public static final String OPERATION_REMOVE = "remove";
-
-    @Autowired
-    InstallItemService installItemService;
-
-    @Autowired
-    ItemService itemService;
 
     @Autowired
     ProvenanceService provenanceService;
