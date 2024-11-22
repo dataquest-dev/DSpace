59ef4d256e6f61e449dbd0eff34d234a21b49ff1
Matus Kasak <matus.kasak@dataquest.sk>
2024-11-22 13:30:58 +0100

    Exclude script from license header

--- dspace-api/src/main/resources/testing/python-script.py
+++ dspace-api/src/main/resources/testing/python-script.py
@@ -1,8 +1,1 @@
-#
-# The contents of this file are subject to the license and copyright
-# detailed in the LICENSE and NOTICE files at the root of the source
-# tree and available online at
-#
-# http://www.dspace.org/license/
-#
 print("Hello world from python")
