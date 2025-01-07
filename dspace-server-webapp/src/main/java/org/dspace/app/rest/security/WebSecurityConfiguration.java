253d401de86c446d97247736214cbb5220e67cd3
milanmajchrak <milan.majchrak@dataquest.sk>
2025-01-07 13:10:21 +0100

    Use Vanilla Shibboleth authentication to see if the error will occur

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityConfiguration.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityConfiguration.java
@@ -142,7 +142,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                              LogoutFilter.class)
             // Add a filter before our shibboleth endpoints to do the authentication based on the data in the
             // HTTP request
-            .addFilterBefore(new ClarinShibbolethLoginFilter("/api/authn/shibboleth", authenticationManager(),
+            .addFilterBefore(new ShibbolethLoginFilter("/api/authn/shibboleth", authenticationManager(),
                                                        restAuthenticationService),
                              LogoutFilter.class)
             //Add a filter before our ORCID endpoints to do the authentication based on the data in the
