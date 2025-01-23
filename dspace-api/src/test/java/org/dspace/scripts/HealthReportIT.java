2edf5ee9de38078ee9bfd3dd30a17737dc8812fa
milanmajchrak <milan.majchrak@dataquest.sk>
2025-01-23 12:21:03 +0100

    Changed the name of the test.

--- dspace-api/src/test/java/org/dspace/scripts/HealthReportIT.java
+++ dspace-api/src/test/java/org/dspace/scripts/HealthReportIT.java
@@ -26,7 +26,7 @@ import org.junit.Test;
  */
 public class HealthReportIT extends AbstractIntegrationTestWithDatabase {
     @Test
-    public void testWithoutProcessToDelete() throws Exception {
+    public void testDefaultHealthcheckRun() throws Exception {
 
         TestDSpaceRunnableHandler testDSpaceRunnableHandler = new TestDSpaceRunnableHandler();
 
