5be957c60c3484820ca5b9241ba6562c954db15e
milanmajchrak <milan.majchrak@dataquest.sk>
2025-02-20 10:54:13 +0100

    Uncomment file header

--- dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.6_2024.10.25__insert_default_licenses.sql
+++ dspace-api/src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V7.6_2024.10.25__insert_default_licenses.sql
@@ -1,11 +1,11 @@
-----
----- The contents of this file are subject to the license and copyright
----- detailed in the LICENSE and NOTICE files at the root of the source
----- tree and available online at
-----
----- http://www.dspace.org/license/
-----
 --
+-- The contents of this file are subject to the license and copyright
+-- detailed in the LICENSE and NOTICE files at the root of the source
+-- tree and available online at
+--
+-- http://www.dspace.org/license/
+--
+
 ----
 ---- Default License Definitions with Associated License Labels and Mappings
 ---- NOTE: Do NOT use this file if your repository already contains licenses.
