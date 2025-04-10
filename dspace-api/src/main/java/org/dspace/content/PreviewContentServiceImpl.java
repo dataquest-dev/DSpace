ce529d454cae3fa7dc452ded87982e6f47418db7
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-04-10 09:30:46 +0300

    removed unused import

--- dspace-api/src/main/java/org/dspace/content/PreviewContentServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/PreviewContentServiceImpl.java
@@ -15,7 +15,6 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.ArrayList;
