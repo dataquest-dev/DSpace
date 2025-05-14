1a1d6a50dc73406bc13c092fa73c278a3985475e
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-05-14 10:55:25 +0300

    removed duplicated condition

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/SuggestionRestController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/repository/SuggestionRestController.java
@@ -262,9 +262,7 @@ public class SuggestionRestController extends AbstractDSpaceRestRepository {
                                           List<VocabularyEntryRest> results) {
         searchResult.getFacetResult(autocompleteCustom).forEach(facetResult -> {
             String displayedValue = facetResult.getDisplayedValue();
-            if (!displayedValue.contains(searchValue) &&
-                    !displayedValue.toLowerCase().contains(searchValue) &&
-                    !displayedValue.toLowerCase().contains(searchValue.toLowerCase())) {
+            if (!displayedValue.toLowerCase().contains(searchValue.toLowerCase())) {
                 return;
             }
             // Create a new VocabularyEntryRest object
