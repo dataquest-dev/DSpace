356d3e7218db093f71f747adb799ec578c6c8f48
milanmajchrak <milan.majchrak@dataquest.sk>
2025-06-05 15:30:12 +0200

    Fetch the ReportResultService from the ContentServiceFactory

--- dspace-api/src/main/java/org/dspace/health/Check.java
+++ dspace-api/src/main/java/org/dspace/health/Check.java
@@ -8,6 +8,7 @@
 package org.dspace.health;
 
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 
 /**
  * Abstract check interface.
@@ -21,6 +22,7 @@ public abstract class Check {
     long took_ = -1L;
     String report_ = null;
     private String errors_ = "";
+    private JSONObject reportJson_ = new JSONObject();
 
     // this method should be overridden
     protected abstract String run(ReportInfo ri);
@@ -60,5 +62,13 @@ public abstract class Check {
 
     public long getTook() {
         return took_;
+    }
+
+    public JSONObject getReportJson() {
+        return reportJson_;
+    }
+
+    public void setReportJson(JSONObject reportJson_) {
+        this.reportJson_ = reportJson_;
     }
 }
