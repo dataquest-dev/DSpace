5831fdab26f34dd986b6e37b62c26808fb0e1a65
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-06-06 10:57:26 +0200

    fix problem with hibernate session

--- dspace-api/src/main/java/org/dspace/content/dao/impl/PreviewContentDAOImpl.java
+++ dspace-api/src/main/java/org/dspace/content/dao/impl/PreviewContentDAOImpl.java
@@ -47,8 +47,7 @@ public class PreviewContentDAOImpl extends AbstractHibernateDAO<PreviewContent> 
         Query query = getHibernateSession(context).createNativeQuery(sql);
         query.setParameter("bitstream_id", bitstream.getID());
         query.setHint("org.hibernate.cacheable", Boolean.TRUE);
-        long count = (long) query.getSingleResult();
-        return count > 0;
+        return count(query) > 0;
     }
 
     @Override
