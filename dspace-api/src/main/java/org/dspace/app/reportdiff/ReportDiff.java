fc2c62fa9583dbb2f59fc33240e224bd1edfb97a
milanmajchrak <milan.majchrak@dataquest.sk>
2025-06-10 14:57:52 +0200

    Working somehow, refactoring needed.

--- dspace-api/src/main/java/org/dspace/app/reportdiff/ReportDiff.java
+++ dspace-api/src/main/java/org/dspace/app/reportdiff/ReportDiff.java
@@ -16,6 +16,7 @@ import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang3.builder.Diff;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.dspace.app.healthreport.HealthReport;
 import org.dspace.content.ReportResult;
 import org.dspace.content.factory.ContentServiceFactory;
 import org.dspace.content.service.ReportResultService;
@@ -39,8 +40,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -56,40 +63,34 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
     private static final Logger log = LogManager.getLogger(ReportDiff.class);
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    private ConfigurationService configurationService = DSpaceServicesFactory.getInstance().getConfigurationService();
     private ReportResultService reportResultService = ContentServiceFactory.getInstance().getReportResultService();
     private EPersonService ePersonService;
-
-    /**
-     * Checks to be performed.
-     */
-//    private static final LinkedHashMap<String, Check> checks = Report.checks();
 
     /**
      * `-i`: Info, show help information.
      */
     private boolean info = false;
 
-//    /**
-//     * `-e`: Email, send report to specified email address.
-//     */
-//    private String[] emails;
-//
-//    /**
-//     * `-c`: Check, perform only specific check by index (0-`getNumberOfChecks()`).
-//     */
-//    private int specificCheck = -1;
-//
-//    /**
-//     * `-f`: For, specify the last N days to consider.
-//     * Default value is set in dspace.cfg.
-//     */
-//    private int forLastNDays = configurationService.getIntProperty("healthcheck.last_n_days");
-//
-//    /**
-//     * `-o`: Output, specify a file to save the report.
-//     */
-//    private String fileName;
+    /**
+     * `-d`: Dates, show all dates that the report was generated for a specific check type.
+     */
+    private boolean showDates = false;
+
+    /**
+     * `-c`: Check, perform only specific check by index (0-`getNumberOfChecks()`).
+     */
+    private int specificCheck = -1;
+
+    /**
+     * `-f`: From, specify the start date for the report.
+     */
+    private Date from = null;
+
+    /**
+     * `-t`: Till, specify the end date for the report.
+     */
+    private Date to = null;
+
 
     @Override
     public ReportDiffScriptConfiguration getScriptConfiguration() {
@@ -106,7 +107,51 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
             return;
         }
 
+        // `-c`: Check, perform only specific check by index (0-`getNumberOfChecks()`).
+        if (commandLine.hasOption('c')) {
+            String checkOption = commandLine.getOptionValue('c');
+            try {
+                specificCheck = Integer.parseInt(checkOption);
+                if (specificCheck < 0 || specificCheck >= HealthReport.getNumberOfChecks()) {
+                    specificCheck = -1;
+                }
+            } catch (NumberFormatException e) {
+                log.info("Invalid value for check. It has to be a number from the displayed range.");
+                return;
+            }
+        }
 
+        // `-d`: Dates, show all dates that the report was generated for a specific check type.
+        if (commandLine.hasOption('d')) {
+            showDates = true;
+            return;
+        }
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+        // `-f`: From, specify the start date for the report.
+        if (commandLine.hasOption('f')) {
+            String fromOption = commandLine.getOptionValue('f');
+            try {
+                LocalDateTime ldt = LocalDateTime.parse(fromOption, formatter);
+                ZonedDateTime zdt = ldt.atZone(ZoneId.systemDefault());
+                from = Date.from(zdt.toInstant());
+            } catch (Exception e) {
+                log.error("Cannot create a Date from the input: {}", fromOption, e);
+                return;
+            }
+        }
+
+        // `-t`: Till, specify the end date for the report.
+        if (commandLine.hasOption('t')) {
+            String toOption = commandLine.getOptionValue('t');
+            try {
+                LocalDateTime ldt = LocalDateTime.parse(toOption, formatter);
+                ZonedDateTime zdt = ldt.atZone(ZoneId.systemDefault());
+                to = Date.from(zdt.toInstant());
+            } catch (Exception e) {
+                log.error("Cannot create a Date from the input: {}", toOption, e);
+            }
+        }
     }
 
     @Override
@@ -115,50 +160,123 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
             printHelp();
             return;
         }
-
         Context context = new Context();
-        try {
-            context.setCurrentUser(ePersonService.find(context, this.getEpersonIdentifier()));
-            // Get the last two report results and compare them
-            // Compare two JSONs
-            // Write the report to the log
-            List<ReportResult> allReports = reportResultService.findAll(context);
-            ReportResult newReport = allReports.get(allReports.size() - 1);
-            ReportResult oldReport = allReports.get(allReports.size() - 2);
-            if (newReport == null || oldReport == null) {
-                handler.logError("No previous report found. Cannot compare reports.");
+
+        if (showDates) {
+            // Show all dates for the specific check type
+
+            try {
+                // Show also report diff type in the format "CheckName - Date"
+                context.setCurrentUser(ePersonService.find(context, this.getEpersonIdentifier()));
+                List<ReportResult> allReports = reportResultService.findAll(context);
+                Map<String, List<DateWithArgs>> reportDatesMap = new HashMap<>();
+
+                for (ReportResult report : allReports) {
+                    String dateStr = report.getLastModified().toString();
+
+                    // Add the date to the list for this report type
+                    reportDatesMap
+                            .computeIfAbsent(report.getType(), k -> new ArrayList<>())
+                            .add(new DateWithArgs(dateStr, report.getArgs()));
+                }
+                // Print the report dates
+                StringBuilder sb = new StringBuilder();
+                sb.append("Report Dates Summary:\n");
+                // Get max 20 dates for each report type
+                reportDatesMap.forEach((reportType, dateWithArgsList) -> {
+                    dateWithArgsList.sort(Comparator.comparing(DateWithArgs::getDate).reversed());
+                    List<DateWithArgs> topDates = dateWithArgsList.size() > 20
+                            ? dateWithArgsList.subList(0, 20)
+                            : dateWithArgsList;
+
+                    sb.append(String.format("Report Type: %s%n", reportType));
+                    sb.append(String.format("%s:%n", reportType));
+                    topDates.forEach(entry -> sb.append(
+                            String.format("  - %s | %s%n", entry.getDate(), entry.getArgs())));
+                });
+                handler.logInfo(sb.toString());
+            } finally {
+                context.complete();
+            }
+            return;
+
+        }
+
+        if (to != null && from != null && to.before(from)) {
+            handler.logError("The 'to' date cannot be before the 'from' date.");
+            return;
+        }
+
+        if (to != null && from == null) {
+            handler.logError("The 'to' date is set, but the 'from' date is not. Please set both dates.");
+            return;
+        }
+
+        if (from != null && to == null) {
+            handler.logError("The 'from' date is set, but the 'to' date is not. Please set both dates.");
+            return;
+        }
+
+        if (to != null && from != null) {
+            // If both dates are set, we need to filter the reports by these dates
+            handler.logInfo(String.format("Filtering reports from %s to %s", from, to));
+
+            ReportResult toReport = null;
+            ReportResult fromReport = null;
+            if (specificCheck != -1) {
+                // If a specific check is set, we need to filter the reports by this check type
+                fromReport = reportResultService.findByLastModifiedAndCheckType(context, from, specificCheck);
+                toReport = reportResultService.findByLastModifiedAndCheckType(context, to, specificCheck);
+            } else {
+                toReport = reportResultService.findByLastModified(context, to);
+                fromReport = reportResultService.findByLastModified(context, from);
+            }
+
+            if (toReport == null || fromReport == null) {
+                handler.logInfo(String.format("No reports found between %s and %s", from, to));
                 return;
             }
-            // The report value is stored in the `value` column as a JSON string.
-            String newReportValue = newReport.getValue();
-            String oldReportValue = oldReport.getValue();
-            if (newReportValue == null || oldReportValue == null) {
-                handler.logError("One of the reports has no value. Cannot compare reports.");
-                return;
+
+            try {
+                context.setCurrentUser(ePersonService.find(context, this.getEpersonIdentifier()));
+                // Get the last two report results and compare them
+                // Compare two JSONs
+                // Write the report to the log
+                // The report value is stored in the `value` column as a JSON string.
+                String toReportValue = toReport.getValue();
+                String fromReportValue = fromReport.getValue();
+                if (toReportValue == null || fromReportValue == null) {
+                    handler.logError("One of the reports has no value. Cannot compare reports.");
+                    return;
+                }
+
+                // Create a diff comparing the two JSONs and the result store as String
+                StringBuilder sbReport = new StringBuilder();
+                sbReport.append("\nReport Diff between last two reports:\n");
+                sbReport.append("The Report type: ").append(toReport.getType()).append("\n");
+                sbReport.append("Command line options:\n");
+                sbReport.append("From Report: ").append(fromReport.getLastModified()).append("\n");
+                sbReport.append("To Report: ").append(toReport.getLastModified()).append("\n\n");
+                sbReport.append("Differences:\n\n");
+                // Compare the two JSONs
+                sbReport.append(generateDiff(fromReportValue, toReportValue));
+
+                handler.logInfo(sbReport.toString());
+            } finally {
+                context.complete();
             }
 
-
-            // Create a diff comparing the two JSONs and the result store as String
-            StringBuilder sbReport = new StringBuilder();
-            sbReport.append("\nReport Diff between last two reports:\n");
-            sbReport.append("The Report type: ").append(newReport.getType()).append("\n");
-            sbReport.append("Command line options:\n");
-//            sbReport.append(printCommandlineOptions());
-            sbReport.append("Last Report: ").append(newReport.getLastModified()).append("\n");
-            sbReport.append("Previous Report: ").append(oldReport.getLastModified()).append("\n\n");
-            sbReport.append("Differences:\n");
-            // Compare the two JSONs
-            sbReport.append(generateDiff(oldReportValue, newReportValue));
-
-            handler.logInfo(sbReport.toString());
-        } finally {
-            context.complete();
         }
     }
 
     @Override
     public void printHelp() {
-        handler.logInfo("HEEEEY");
+        handler.printHelp(getScriptConfiguration().getOptions(), getScriptConfiguration().getName());
+        handler.logInfo("This script compares two health reports and shows the differences between them.");
+        handler.logInfo("You can specify the 'from' and 'to' dates to compare reports from specific dates.");
+        handler.logInfo("If you want to see all available report dates, use the '-d' option.");
+        handler.logInfo("If you want to compare a specific check, use the '-c' option with the check index, " +
+                "in this case you must also specify the `from` and `to` dates.");
     }
 
 
@@ -196,7 +314,7 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
                     // Lookup the old value from oldNode:
                     JsonNode oldValue = oldNode.at(path);
                     sb.append(String.format(
-                            "REPLACE at %s: %s -> %s%n",
+                            "- REPLACE at %s: %s -> %s%n",
                             path,
                             nodeToEscapedString(oldValue),
                             nodeToEscapedString(newValue)
@@ -206,7 +324,7 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
                 case "add": {
                     JsonNode addedValue = op.get("value");
                     sb.append(String.format(
-                            "ADD     at %s: %s%n",
+                            "- ADD     at %s: %s%n",
                             path,
                             nodeToEscapedString(addedValue)
                     ));
@@ -215,7 +333,7 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
                 case "remove": {
                     JsonNode removedValue = oldNode.at(path);
                     sb.append(String.format(
-                            "REMOVE  at %s: %s%n",
+                            "- REMOVE  at %s: %s%n",
                             path,
                             nodeToEscapedString(removedValue)
                     ));
@@ -224,7 +342,7 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
                 case "move": {
                     String from = op.get("from").asText();
                     sb.append(String.format(
-                            "MOVE    from %s to %s%n",
+                            "- MOVE    from %s to %s%n",
                             from,
                             path
                     ));
@@ -233,7 +351,7 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
                 case "copy": {
                     String fromCopy = op.get("from").asText();
                     sb.append(String.format(
-                            "COPY    from %s to %s%n",
+                            "- COPY    from %s to %s%n",
                             fromCopy,
                             path
                     ));
@@ -242,7 +360,7 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
                 case "test": {
                     JsonNode testValue = op.get("value");
                     sb.append(String.format(
-                            "TEST    at %s: must equal %s%n",
+                            "- TEST    at %s: must equal %s%n",
                             path,
                             nodeToEscapedString(testValue)
                     ));
@@ -273,23 +391,22 @@ public class ReportDiff extends DSpaceRunnable<ReportDiffScriptConfiguration> {
         // In particular, a text node will come out as "\"some text\\n\"" (with \\n escaped).
         return node.toString();
     }
+}
 
-//    /**
-//     * Convert checks names to string.
-//     */
-//    private String checksNamesToString() {
-//        StringBuilder names = new StringBuilder();
-//        int pos = 0;
-//        for (String name : checks.keySet()) {
-//            names.append(String.format("   %d. %s\n", pos++, name));
-//        }
-//        return names.toString();
-//    }
-//
-//    /**
-//     * Get the number of checks. This is used for the `-c` option.
-//     */
-//    public static int getNumberOfChecks() {
-//        return checks.size();
-//    }
+class DateWithArgs {
+    private final String date;
+    private final String args;
+
+    public DateWithArgs(String date, String args) {
+        this.date = date;
+        this.args = args;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getArgs() {
+        return args;
+    }
 }
