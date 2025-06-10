6805000e9b630c42c80e2cc3988548e5cb039ee8
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-06-10 07:14:30 +0200

    named constant by upper case

--- dspace-api/src/main/java/org/dspace/content/PreviewContentServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/content/PreviewContentServiceImpl.java
@@ -72,7 +72,7 @@ public class PreviewContentServiceImpl implements PreviewContentService {
     // the database from being overloaded with large amounts of data.
     private static final int MAX_PREVIEW_COUNT_LENGTH = 2000;
     // Initial capacity for the list of extracted file paths, set to 200 based on typical archive file counts.
-    private int estimatedFileCount = 200;
+    private int ESTIMATED_FILE_COUNT = 200;
 
     // Configured ZIP file preview limit (default: 1000) - if the ZIP file contains more files, it will be truncated
     @Value("${file.preview.zip.limit.length:1000}")
@@ -448,7 +448,7 @@ public class PreviewContentServiceImpl implements PreviewContentService {
      * @return an XML string representing the extracted file paths
      */
     private String extractFile(File file, String fileType) throws Exception {
-        List<String> filePaths = new ArrayList<>(estimatedFileCount);
+        List<String> filePaths = new ArrayList<>(ESTIMATED_FILE_COUNT);
         // Process the file based on its type
         if (ARCHIVE_TYPE_TAR.equals(fileType)) {
             processTarFile(filePaths, file);
