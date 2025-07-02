81ab9d5769609eebcda3cc8039f713c3fcb744b3
Kim Shepherd <kim@shepherd.nz>
2025-07-02 10:39:50 +0200

    Safe and consistent XML entity handling in parsers
    (cherry picked from commit f7dcbf1b44fb2d973000cedadb4d1f5a48bd70a1)

--- dspace-api/src/main/java/org/dspace/app/itemimport/ItemImportServiceImpl.java
+++ dspace-api/src/main/java/org/dspace/app/itemimport/ItemImportServiceImpl.java
@@ -50,7 +50,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import javax.mail.MessagingException;
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPath;
@@ -70,6 +69,7 @@ import org.apache.logging.log4j.Logger;
 import org.dspace.app.itemimport.service.ItemImportService;
 import org.dspace.app.util.LocalSchemaFilenameFilter;
 import org.dspace.app.util.RelationshipUtils;
+import org.dspace.app.util.XMLUtils;
 import org.dspace.authorize.AuthorizeException;
 import org.dspace.authorize.ResourcePolicy;
 import org.dspace.authorize.service.AuthorizeService;
@@ -188,6 +188,8 @@ public class ItemImportServiceImpl implements ItemImportService, InitializingBea
     protected ClarinLicenseService clarinLicenseService;
     @Autowired(required = true)
     protected ClarinLicenseResourceMappingService clarinLicenseResourceMappingService;
+
+    protected DocumentBuilder builder;
 
     protected String tempWorkDir;
 
@@ -1922,9 +1924,7 @@ public class ItemImportServiceImpl implements ItemImportService, InitializingBea
      */
     protected Document loadXML(String filename) throws IOException,
         ParserConfigurationException, SAXException {
-        DocumentBuilder builder = DocumentBuilderFactory.newInstance()
-                                                        .newDocumentBuilder();
-
+        DocumentBuilder builder = XMLUtils.getDocumentBuilder();
         return builder.parse(new File(filename));
     }
 
