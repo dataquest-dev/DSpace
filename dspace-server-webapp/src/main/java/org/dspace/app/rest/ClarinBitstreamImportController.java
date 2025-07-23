253decec8eeb4ffe99198651e69dba53a2c4f8e9
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-07-23 14:42:19 +0200

    added removed line

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/ClarinBitstreamImportController.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/ClarinBitstreamImportController.java
@@ -180,6 +180,7 @@ public class ClarinBitstreamImportController {
                             primaryBundleUUID, e);
                 }
             }
+            
             log.info("Going to update bitstream with UUID: " + bitstream.getID());
             bitstreamService.update(context, bitstream);
             // If bitstream is deleted make it deleted
