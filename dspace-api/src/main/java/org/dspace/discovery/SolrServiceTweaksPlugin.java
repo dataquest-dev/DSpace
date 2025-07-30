0f52e05724597f9287f0e1d19f5ac18bcf2bbcf9
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-07-30 13:24:52 +0200

    Removed preference for title matching.

--- dspace-api/src/main/java/org/dspace/discovery/SolrServiceTweaksPlugin.java
+++ dspace-api/src/main/java/org/dspace/discovery/SolrServiceTweaksPlugin.java
@@ -11,7 +11,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.dspace.core.Context;
-import org.springframework.beans.factory.annotation.Value;
 
 /**
  * A DSpace Discovery plugin that customizes Solr queries to prioritize newer versions of items.
@@ -19,27 +18,13 @@ import org.springframework.beans.factory.annotation.Value;
 public class SolrServiceTweaksPlugin implements SolrServiceSearchPlugin {
     private static final Logger log = LogManager.getLogger(SolrServiceTweaksPlugin.class);
 
-    @Value("${solr.boost.title:2.0}")
-    private float titleBoostValue;
-
     /**
-     * Enhances Solr search by boosting matches in the "title" field
-     * to prioritize them in the results. Additionally, results are
-     * sorted by "versionNumber" in descending order to show the most
+     * Results are sorted by "versionNumber" in descending order to show the most
      * recent versions first.
      */
     @Override
     public void additionalSearchParameters(Context context, DiscoverQuery discoveryQuery, SolrQuery solrQuery) {
-        String userQuery = discoveryQuery.getQuery();
-        String query = String.format(
-                "title:(%s)^%.1f",
-                userQuery,
-                titleBoostValue
-        );
-
-        solrQuery.setQuery(query);
         solrQuery.setSort("versionNumber", SolrQuery.ORDER.desc);
-
         log.debug("Modified Solr query: {}", solrQuery);
     }
 }
