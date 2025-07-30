8516ed3a03c764e3a93f39782496b825edd77617
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-07-30 13:52:38 +0200

    replaced * by assert funct /checkstyle

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/MetadataBitstreamControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/MetadataBitstreamControllerIT.java
@@ -7,7 +7,9 @@
  */
 package org.dspace.app.rest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
