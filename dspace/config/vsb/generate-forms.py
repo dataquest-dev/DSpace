905a38b23caa6bc589eb11ab2882e6e44bf1dbbf
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-10-07 14:26:50 +0200

    added .

--- dspace/config/vsb/generate-forms.py
+++ dspace/config/vsb/generate-forms.py
@@ -12,7 +12,7 @@ from datetime import datetime
 import shutil
 from pathlib import Path
 import re
-from vsb_config import VSB_BASE_URL, VSB_TEST_URL
+from .vsb_config import VSB_BASE_URL, VSB_TEST_URL
 
 # Faculty codes
 FACULTIES = ["FAST", "FBI", "FS", "FEI", "HGF", "FMT", "EKF", "USP", "9270", "AUD"]
