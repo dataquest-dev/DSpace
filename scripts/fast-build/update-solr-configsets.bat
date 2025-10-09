729c1fb4656ea84be31013c0cde4c70a73628330
milanmajchrak <milan.majchrak@dataquest.sk>
2025-10-09 09:17:27 +0200

    Reverted changes for solr-update script

--- scripts/fast-build/update-solr-configsets.bat
+++ scripts/fast-build/update-solr-configsets.bat
@@ -1,10 +1,5 @@
 call ..\envs\__basic.bat
 
-:: Remove old Solr config sets
-rd /s /q %dspace_solr%server\solr\configsets\authority
-rd /s /q %dspace_solr%server\solr\configsets\oai
-rd /s /q %dspace_solr%server\solr\configsets\search
-rd /s /q %dspace_solr%server\solr\configsets\statistics
-
-:: Copy new Solr configuration
+rm -rf %dspace_solr%server\solr\configsets\authority %dspace_solr%server\solr\configsets\oai dspace_solr%server\solr\configsets\search dspace_solr%server\solr\configsets\statistics
 xcopy /e /h /i /q /y %dspace_application%solr\ %dspace_solr%server\solr\configsets\
+
