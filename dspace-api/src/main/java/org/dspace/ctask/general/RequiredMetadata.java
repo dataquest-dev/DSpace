ufal/clarin-dspace

issue 1233: requiredmetadata curation task - fix NPE + few other issues (#1245)
* issue 1233: requiredmetadata curation task - fix NPE + few other issues

* use constant rather that hardcoded value

* resolve pull request comments

* integration test

* attempt to fix failing test

* update test to test usecase with workspace item

* cleanup handles properly in Integration test (no avoid clashing handles with other potential test)

* pull check for null outside of try catch block

dspace-api/src/main/java/org/dspace/ctask/general/RequiredMetadata.java
@@ -8,22 +8,35 @@
 package org.dspace.ctask.general;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 import org.dspace.app.util.DCInput;
 import org.dspace.app.util.DCInputSet;
 import org.dspace.app.util.DCInputsReader;
 import org.dspace.app.util.DCInputsReaderException;
+import org.dspace.content.Collection;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
+import org.dspace.content.MetadataSchemaEnum;
 import org.dspace.content.MetadataValue;
+import org.dspace.content.WorkspaceItem;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.WorkspaceItemService;
 import org.dspace.core.Constants;
+import org.dspace.core.Context;
 import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
 import org.dspace.curate.Suspendable;
+import org.dspace.workflow.WorkflowItem;
+import org.dspace.workflow.WorkflowItemService;
+import org.dspace.workflow.factory.WorkflowServiceFactory;
 
 /**
  * RequiredMetadata task compares item metadata with fields
@@ -33,12 +46,15 @@ import org.dspace.curate.Suspendable;
  *
  * @author richardrodgers
  */
-@Suspendable
+@Suspendable(statusCodes = {Curator.CURATE_ERROR})
 public class RequiredMetadata extends AbstractCurationTask {
     // map of DCInputSets
     protected DCInputsReader reader = null;
     // map of required fields
-    protected Map<String, List<String>> reqMap = new HashMap<String, List<String>>();
+    protected Map<ReqKey, List<String>> reqMap = new HashMap<>();
+
+    private WorkflowItemService<?> workflowItemService;
+    private WorkspaceItemService workspaceItemService;
 
     @Override
     public void init(Curator curator, String taskId) throws IOException {
@@ -48,6 +64,8 @@ public class RequiredMetadata extends AbstractCurationTask {
         } catch (DCInputsReaderException dcrE) {
             throw new IOException(dcrE.getMessage(), dcrE);
         }
+        this.workflowItemService = WorkflowServiceFactory.getInstance().getWorkflowItemService();
+        this.workspaceItemService = ContentServiceFactory.getInstance().getWorkspaceItemService();
     }
 
     /**
@@ -69,7 +87,34 @@ public class RequiredMetadata extends AbstractCurationTask {
                     handle = "in workflow";
                 }
                 sb.append("Item: ").append(handle);
-                for (String req : getReqList(item.getOwningCollection().getHandle())) {
+
+                Collection collection = item.getOwningCollection();
+
+                // when the owning collection is null it may be the case
+                // when the item is a workspace item or a workflow item
+                if (collection == null) {
+                    try {
+                        Context context = Curator.curationContext();
+                        if (itemService.isInProgressSubmission(context, item)) {
+                            WorkflowItem workflowItem = workflowItemService.findByItem(context, item);
+                            if (workflowItem != null) {
+                                collection = workflowItem.getCollection();
+                            } else {
+                                WorkspaceItem workspaceItem = workspaceItemService.findByItem(context, item);
+                                if (workspaceItem != null) {
+                                    collection = workspaceItem.getCollection();
+                                }
+                            }
+                        }
+                    } catch (SQLException ex) {
+                        throw new IOException(ex.getMessage(), ex);
+                    }
+                }
+
+                String resourceType = itemService.getMetadataFirstValue(
+                        item, MetadataSchemaEnum.DC.getName(), "type", null, Item.ANY);
+
+                for (String req : getReqList(collection.getHandle(), resourceType)) {
                     List<MetadataValue> vals = itemService.getMetadataByMetadataString(item, req);
                     if (vals.size() == 0) {
                         sb.append(" missing required field: ").append(req);
@@ -91,18 +136,28 @@ public class RequiredMetadata extends AbstractCurationTask {
         }
     }
 
-    protected List<String> getReqList(String handle) throws DCInputsReaderException {
-        List<String> reqList = reqMap.get(handle);
+    /**
+     * Get the list of required metadata for given collection and item resource type.
+     * The list is obtained from submission-forms.xml configuration file.
+     * <p>
+     * In order to avoid required metadata list calculation repeatedly,
+     * the lists are cached into reqMap object for given collection handle and item resource type.
+     *
+     * @param collectionHandle item's owning collection handle
+     * @param resourceType item resource type
+     * @return the list of required metadata
+     * @throws DCInputsReaderException when DCInputsReader error occurs
+     */
+    private List<String> getReqList(String collectionHandle, String resourceType) throws DCInputsReaderException {
+        ReqKey reqKey = new ReqKey(collectionHandle, resourceType);
+        List<String> reqList = reqMap.get(reqKey);
         if (reqList == null) {
-            reqList = reqMap.get("default");
-        }
-        if (reqList == null) {
-            reqList = new ArrayList<String>();
-            List<DCInputSet> inputSet = reader.getInputsByCollectionHandle(handle);
+            Set<String> reqSet = new LinkedHashSet<>();
+            List<DCInputSet> inputSet = reader.getInputsByCollectionHandle(collectionHandle);
             for (DCInputSet inputs : inputSet) {
                 for (DCInput[] row : inputs.getFields()) {
                     for (DCInput input : row) {
-                        if (input.isRequired()) {
+                        if (input.isRequired() && ((resourceType == null) || input.isAllowedFor(resourceType))) {
                             StringBuilder sb = new StringBuilder();
                             sb.append(input.getSchema()).append(".");
                             sb.append(input.getElement()).append(".");
@@ -111,13 +166,38 @@ public class RequiredMetadata extends AbstractCurationTask {
                                 qual = "";
                             }
                             sb.append(qual);
-                            reqList.add(sb.toString());
+                            reqSet.add(sb.toString());
                         }
                     }
                 }
-                reqMap.put(inputs.getFormName(), reqList);
             }
+            reqList = new ArrayList<>(reqSet);
+            reqMap.put(reqKey, reqList);
         }
         return reqList;
+    }
+
+    protected static class ReqKey {
+        private final String handle;
+        private final String resourceType;
+
+        protected ReqKey(String handle, String resourceType) {
+            this.handle = handle;
+            this.resourceType = resourceType;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ReqKey reqKey = (ReqKey) o;
+            return Objects.equals(handle, reqKey.handle) && Objects.equals(resourceType, reqKey.resourceType);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(handle, resourceType);
+        }
     }
 }

dataquest-dev/DSpace

dspace-api/src/main/java/org/dspace/ctask/general/RequiredMetadata.java
@@ -8,10 +8,14 @@
 package org.dspace.ctask.general;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 import org.dspace.app.util.DCInput;
 import org.dspace.app.util.DCInputSet;
@@ -20,11 +24,19 @@ import org.dspace.app.util.DCInputsReaderException;
 import org.dspace.content.Collection;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
+import org.dspace.content.MetadataSchemaEnum;
 import org.dspace.content.MetadataValue;
+import org.dspace.content.WorkspaceItem;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.WorkspaceItemService;
 import org.dspace.core.Constants;
+import org.dspace.core.Context;
 import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
 import org.dspace.curate.Suspendable;
+import org.dspace.workflow.WorkflowItem;
+import org.dspace.workflow.WorkflowItemService;
+import org.dspace.workflow.factory.WorkflowServiceFactory;
 
 /**
  * RequiredMetadata task compares item metadata with fields
@@ -34,12 +46,15 @@ import org.dspace.curate.Suspendable;
  *
  * @author richardrodgers
  */
-@Suspendable
+@Suspendable(statusCodes = {Curator.CURATE_ERROR})
 public class RequiredMetadata extends AbstractCurationTask {
     // map of DCInputSets
     protected DCInputsReader reader = null;
     // map of required fields
-    protected Map<String, List<String>> reqMap = new HashMap<String, List<String>>();
+    protected Map<ReqKey, List<String>> reqMap = new HashMap<>();
+
+    private WorkflowItemService<?> workflowItemService;
+    private WorkspaceItemService workspaceItemService;
 
     @Override
     public void init(Curator curator, String taskId) throws IOException {
@@ -49,6 +64,8 @@ public class RequiredMetadata extends AbstractCurationTask {
         } catch (DCInputsReaderException dcrE) {
             throw new IOException(dcrE.getMessage(), dcrE);
         }
+        this.workflowItemService = WorkflowServiceFactory.getInstance().getWorkflowItemService();
+        this.workspaceItemService = ContentServiceFactory.getInstance().getWorkspaceItemService();
     }
 
     /**
@@ -70,7 +87,34 @@ public class RequiredMetadata extends AbstractCurationTask {
                     handle = "in workflow";
                 }
                 sb.append("Item: ").append(handle);
-                for (String req : getReqList(item.getOwningCollection())) {
+
+                Collection collection = item.getOwningCollection();
+
+                // when the owning collection is null it may be the case
+                // when the item is a workspace item or a workflow item
+                if (collection == null) {
+                    try {
+                        Context context = Curator.curationContext();
+                        if (itemService.isInProgressSubmission(context, item)) {
+                            WorkflowItem workflowItem = workflowItemService.findByItem(context, item);
+                            if (workflowItem != null) {
+                                collection = workflowItem.getCollection();
+                            } else {
+                                WorkspaceItem workspaceItem = workspaceItemService.findByItem(context, item);
+                                if (workspaceItem != null) {
+                                    collection = workspaceItem.getCollection();
+                                }
+                            }
+                        }
+                    } catch (SQLException ex) {
+                        throw new IOException(ex.getMessage(), ex);
+                    }
+                }
+
+                String resourceType = itemService.getMetadataFirstValue(
+                        item, MetadataSchemaEnum.DC.getName(), "type", null, Item.ANY);
+
+                for (String req : getReqList(collection, resourceType)) {
                     List<MetadataValue> vals = itemService.getMetadataByMetadataString(item, req);
                     if (vals.size() == 0) {
                         sb.append(" missing required field: ").append(req);
@@ -92,18 +136,29 @@ public class RequiredMetadata extends AbstractCurationTask {
         }
     }
 
-    protected List<String> getReqList(Collection collection) throws DCInputsReaderException {
-        List<String> reqList = reqMap.get(collection.getHandle());
+    /**
+     * Get the list of required metadata for given collection and item resource type.
+     * The list is obtained from submission-forms.xml configuration file.
+     * <p>
+     * In order to avoid required metadata list calculation repeatedly,
+     * the lists are cached into reqMap object for given collection handle and item resource type.
+     *
+     * @param collection item's owning collection
+     * @param resourceType item resource type
+     * @return the list of required metadata
+     * @throws DCInputsReaderException when DCInputsReader error occurs
+     */
+    private List<String> getReqList(Collection collection, String resourceType) throws DCInputsReaderException {
+        String collectionHandle = collection.getHandle();
+        ReqKey reqKey = new ReqKey(collectionHandle, resourceType);
+        List<String> reqList = reqMap.get(reqKey);
         if (reqList == null) {
-            reqList = reqMap.get("default");
-        }
-        if (reqList == null) {
-            reqList = new ArrayList<String>();
+            Set<String> reqSet = new LinkedHashSet<>();
             List<DCInputSet> inputSet = reader.getInputsByCollection(collection);
             for (DCInputSet inputs : inputSet) {
                 for (DCInput[] row : inputs.getFields()) {
                     for (DCInput input : row) {
-                        if (input.isRequired()) {
+                        if (input.isRequired() && ((resourceType == null) || input.isAllowedFor(resourceType))) {
                             StringBuilder sb = new StringBuilder();
                             sb.append(input.getSchema()).append(".");
                             sb.append(input.getElement()).append(".");
@@ -112,13 +167,38 @@ public class RequiredMetadata extends AbstractCurationTask {
                                 qual = "";
                             }
                             sb.append(qual);
-                            reqList.add(sb.toString());
+                            reqSet.add(sb.toString());
                         }
                     }
                 }
-                reqMap.put(inputs.getFormName(), reqList);
             }
+            reqList = new ArrayList<>(reqSet);
+            reqMap.put(reqKey, reqList);
         }
         return reqList;
+    }
+
+    protected static class ReqKey {
+        private final String handle;
+        private final String resourceType;
+
+        protected ReqKey(String handle, String resourceType) {
+            this.handle = handle;
+            this.resourceType = resourceType;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ReqKey reqKey = (ReqKey) o;
+            return Objects.equals(handle, reqKey.handle) && Objects.equals(resourceType, reqKey.resourceType);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(handle, resourceType);
+        }
     }
 }