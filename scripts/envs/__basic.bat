e485980f20ee06f653a292fd7150f156e14441c8
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-10-23 14:22:46 +0200

    changed old version of apache-tomcat to newer - used by DSpace v9

--- scripts/envs/__basic.bat
+++ scripts/envs/__basic.bat
@@ -1,5 +1,5 @@
 set dspace_source=C:\workspace\DSpace
-set tomcat=C:\workspace\apache-tomcat-9.0.78
+set tomcat=C:\workspace\apache-tomcat-10.1.48
 set dspace_application=C:\dspace
 set m2_source=%USERPROFILE%\.m2
 set dspace_solr=C:\workspace\solr
