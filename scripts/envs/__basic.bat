92411bab2d4f17e38dc15d97d2cd39d2c3d40e83
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2025-10-23 08:58:57 +0200

    copied our building script into mendelu project

---
+++ scripts/envs/__basic.bat
@@ -0,0 +1,5 @@
+set dspace_source=C:\workspace\DSpace
+set tomcat=C:\workspace\apache-tomcat-9.0.78
+set dspace_application=C:\dspace
+set m2_source=%USERPROFILE%\.m2
+set dspace_solr=C:\workspace\solr
