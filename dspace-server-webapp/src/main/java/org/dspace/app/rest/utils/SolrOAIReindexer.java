f0ee956f0c4b4d3a0972c96383901c4cfb944c41
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-11-05 10:20:24 +0100

    removed empty line

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/utils/SolrOAIReindexer.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/utils/SolrOAIReindexer.java
@@ -25,7 +25,6 @@ import javax.xml.stream.XMLStreamException;
 import com.lyncode.xoai.dataprovider.exceptions.WritingXmlException;
 import com.lyncode.xoai.dataprovider.xml.XmlOutputContext;
 import com.lyncode.xoai.dataprovider.xml.xoai.Metadata;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.solr.client.solrj.SolrClient;
