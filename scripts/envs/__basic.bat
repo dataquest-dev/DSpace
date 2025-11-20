f4ac9719111a37f1bf64f9afc9886bbf5e9c4565
milanmajchrak <milan.majchrak@dataquest.sk>
2025-11-20 09:09:11 +0100

    Removed file

--- scripts/envs/__basic.bat
+++
@@ -1,5 +0,0 @@
-rem Set those paths to relevant places in your computer, copy this file and rename the copy to "__basic.bat"
-set dspace_source=C:\dspace-be\
-set tomcat=C:\workspace\apps\apache-tomcat-9.0.53\bin\
-set dspace_application=C:\dspace\
-set m2_source=%USERPROFILE%\.m2
