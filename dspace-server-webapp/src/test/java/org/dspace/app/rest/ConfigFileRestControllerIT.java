273c8656d96ead55f63a5c32e144784cacd36b2c
Paurikova2 <michaela.paurikova@dataquest.sk>
2025-12-09 15:16:54 +0100

    set up back config property config.admin.updateable.files

--- dspace-server-webapp/src/test/java/org/dspace/app/rest/ConfigFileRestControllerIT.java
+++ dspace-server-webapp/src/test/java/org/dspace/app/rest/ConfigFileRestControllerIT.java
@@ -15,6 +15,7 @@ import org.dspace.app.rest.test.AbstractControllerIntegrationTest;
 import org.dspace.builder.EPersonBuilder;
 import org.dspace.eperson.EPerson;
 import org.dspace.services.ConfigurationService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,10 +31,24 @@ public class ConfigFileRestControllerIT extends AbstractControllerIntegrationTes
     @Autowired
     private ConfigurationService configurationService;
 
+    private String originalConfigValue;
+
     @Before
     public void setup() throws Exception {
+        // Save original configuration value
+        originalConfigValue = configurationService.getProperty("config.admin.updateable.files");
+        
         configurationService.setProperty("config.admin.updateable.files",
                 "dspace.cfg,local.cfg,item-submission.xml,submission-forms.xml");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Restore original configuration value to ensure test isolation
+        if (originalConfigValue != null) {
+            configurationService.setProperty("config.admin.updateable.files", originalConfigValue);
+        }
+        // Note: We don't explicitly set to null as ConfigurationService handles missing properties appropriately
     }
 
 
