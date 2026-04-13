34ebbfdf7f088a4aba59517ab4ef2097729d6559
Juraj Roka <95219754+jr-rk@users.noreply.github.com>
2026-04-13 17:40:56 +0200

    add helpful scripts folder

---
+++ scripts/envs/__basic.bat
@@ -0,0 +1,5 @@
+set dspace_source=E:\workspace\DSpace
+set tomcat=E:\workspace\apache-tomcat-10.1.48
+set dspace_application=E:\dspace
+set m2_source=%USERPROFILE%\.m2
+set dspace_solr=E:\workspace\solr
